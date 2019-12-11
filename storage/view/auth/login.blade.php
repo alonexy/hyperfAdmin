5d6a56516f8cabe63344fac240477c3426bcb188
alonexy <hpl1124@126.com>
2019-12-11 19:29:44 +0800

    update login blade

--- storage/view/auth/login.blade.php
+++ storage/view/auth/login.blade.php
@@ -1,48 +1,94 @@
-<!DOCTYPE HTML>
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-    <title>HF-Admin/Auth</title>
-    <!-- Custom Theme files -->
-    <link href="/static/admin_login/css/style.css" rel="stylesheet" type="text/css" media="all"/>
-    <!-- Custom Theme files -->
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <meta name="keywords" content="Login form web template, Sign up Web Templates, Flat Web Templates, Login signup Responsive web template, Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyErricsson, Motorola web design" />
-    <!--Google Fonts-->
-    <link href='/static/admin_login/css/a.css' rel='stylesheet' type='text/css'>
-    <link href='/static/admin_login/css/b.css' rel='stylesheet' type='text/css'>
-    <!--Google Fonts-->
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HF-Admin</title>
+    <meta name="description" content="Custom Login Form Styling with CSS3" />
+    <meta name="keywords" content="css3, login, form, custom, input, submit, button, html5, placeholder" />
+    <meta name="author" content="Codrops" />
+    <link rel="shortcut icon" href="/favicon.ico">
+    <link rel="stylesheet" type="text/css" href="/static/login/css/style.css" />
+    <script src="/static/login/modernizr.custom.63321.js"></script>
+    <!--[if lte IE 7]><style>.main{display:none;} .support-note .note-ie{display:block;}</style><![endif]-->
+    <style>
+        body {
+            background: #e1c192 url("/static/login/images/wood_pattern.jpg");
+        }
+    </style>
 </head>
 <body>
-<div class="login">
-    <h2>HF-Admin</h2>
-    <div class="login-top">
-        <h1>登陆</h1>
-        @if (!empty($errors))
-            <div class="alert alert-error" style="text-align: center">
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-        @endif
-        <form method="POST" action="/auth/login">
-            <input type="email" name="email" value="User Email" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'User Email';}">
-            <input type="password" name="passwd" value="PASS"  id="password" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'password';}">
-            <br />
-            <input type="checkbox" name="remember"> 记住我
-        <div class="forgot">
-            <input type="submit" value="Login" >
+
+<div class="container">
+    @if (!empty($errors))
+        <div style="background-color: #c70a1b;color: oldlace;">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li style="text-align: center"><b>{{ $error }}</b></li>
+                @endforeach
+            </ul>
         </div>
-        </form>
-    </div>
-</div>
-<script>
-    @if ($status == 1)
-        window.location.href="/admin";
     @endif
+    <section class="main">
+        <form class="form-2" method="POST" action="/auth/login">
+            <h1><span class="log-in">Log in</span></h1>
+            <p class="float">
+                <label for="login"><i class="icon-user"></i>USER</label>
+                <input type="text" name="email" placeholder="Username or email" value="">
+            </p>
+            <p class="float">
+                <label for="password"><i class="icon-lock"></i>Password</label>
+                <input type="password" name="passwd" placeholder="Password" class="showpassword">
+            </p>
+            <p class="clearfix">
+                <a href="#" class="log-twitter">Log in with Wechat</a>
+                <input type="submit" name="submit" value="登陆">
+            </p>
+        </form>​​
+    </section>
+
+</div>
+<!-- jQuery if needed -->
+<script type="text/javascript" src="/static/jquery1_8.js"></script>
+<script src="/layer/layer.js"></script>
+<script type="text/javascript">
+    $(function(){
+        @if ($status == 1)
+              window.location.href="/admin";
+        @endif
+            $(".showpassword").each(function(index,input) {
+            var $input = $(input);
+            $("<p class='opt'/>").append(
+                    $("<input type='checkbox' class='showpasswordcheckbox' id='showPassword' />").click(function() {
+                        var change = $(this).is(":checked") ? "text" : "password";
+                        var rep = $("<input placeholder='Password' type='" + change + "' />")
+                                .attr("id", $input.attr("id"))
+                                .attr("name", $input.attr("name"))
+                                .attr('class', $input.attr('class'))
+                                .val($input.val())
+                                .insertBefore($input);
+                        $input.remove();
+                        $input = rep;
+                    })
+            ).append($("<label for='showPassword'/>").text("Show password")).insertAfter($input.parent());
+        });
+
+        $('#showPassword').click(function(){
+            if($("#showPassword").is(":checked")) {
+                $('.icon-lock').addClass('icon-unlock');
+                $('.icon-unlock').removeClass('icon-lock');
+            } else {
+                $('.icon-unlock').addClass('icon-lock');
+                $('.icon-lock').removeClass('icon-unlock');
+            }
+        });
+        $(".log-twitter").bind({
+            click:function(){
+                layer.msg('Coding');
+            }
+        })
+    });
 </script>
 </body>
 </html>
