021721271078c0b948bb1d4c79e25cd5508c1f60
alonexy <hpl1124@126.com>
2020-03-25 18:00:44 +0800

    add Test

--- config/autoload/server.php
+++ config/autoload/server.php
@@ -19,7 +19,7 @@ return [
         [
             'name' => 'http',
             'type' => Server::SERVER_HTTP,
-            'host' => '0.0.0.0',
+            'host' => "127.0.0.1",
             'port' => 9501,
             'sock_type' => SWOOLE_SOCK_TCP,
             'callbacks' => [
